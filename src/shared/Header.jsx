0205a0d9079cbe316196e0c4872a87184be32a93
Дмитрий Белых <ponyashcat228@gmail.com>
2021-10-26 15:00:10 +0300

    tst css

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { hot } from "react-hot-loader/root";
 import styles from "./header.css";
 export function HeaderComponent() {
+  console.log(styles, styles.example);
   return (
     <header>
       <h1 className={styles.example}>Reddit for our own Test</h1>
